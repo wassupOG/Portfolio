c3567e63c67eb60294fb340fbeffd41101e4f784
Ilya Naumov <112390560+wassupOG@users.noreply.github.com>
2023-08-23 14:10:33 +0300

    first

---
+++ static/script.js
@@ -0,0 +1,36 @@
+const MODE_SWITCH = document.querySelector("#mode-switch")
+const LANG_SWITCH = document.querySelector("#lang-switch")
+const BODY = document.querySelector("body")
+const ENG = document.querySelector("#ENG")
+const RU = document.querySelector("#RU")
+
+MODE_SWITCH.addEventListener("change", (e) => {
+  if (e.target.value === "system") {
+    BODY.classList.remove(...BODY.classList)
+  } else {
+    BODY.classList.remove(...BODY.classList)
+    BODY.classList.add(`${e.target.value}`)
+  }
+})
+
+const preferredLanguage = navigator.language
+
+if (preferredLanguage === "en-US") {
+  RU.style.display = "none"
+  LANG_SWITCH.textContent = "ENG"
+} else {
+  ENG.style.display = "none"
+  LANG_SWITCH.textContent = "RU"
+}
+
+LANG_SWITCH.addEventListener("click", (e) => {
+  if (e.target.textContent === "ENG") {
+    ENG.style.display = "none"
+    RU.style.display = "block"
+    e.target.textContent = "RU"
+  } else {
+    RU.style.display = "none"
+    ENG.style.display = "block"
+    e.target.textContent = "ENG"
+  }
+})
