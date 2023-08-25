66df939d898db8c18203b6c79acbc40e17070188
Ilya Naumov <112390560+wassupOG@users.noreply.github.com>
2023-08-25 11:33:45 +0300

    Small rework

--- static/script.js
+++ static/script.js
@@ -1,36 +1,35 @@
-const MODE_SWITCH = document.querySelector("#mode-switch")
-const LANG_SWITCH = document.querySelector("#lang-switch")
-const BODY = document.querySelector("body")
-const ENG = document.querySelector("#ENG")
-const RU = document.querySelector("#RU")
+const MODE_SWITCH = document.querySelector("#mode-switch");
+const LANG_SWITCH = document.querySelector("#lang-switch");
+const BODY = document.querySelector("body");
+const ENG = document.querySelector("#ENG");
+const RU = document.querySelector("#RU");
+const PREFERRED_LANGUAGE = navigator.language;
 
 MODE_SWITCH.addEventListener("change", (e) => {
   if (e.target.value === "system") {
-    BODY.classList.remove(...BODY.classList)
+    BODY.classList.remove(...BODY.classList);
   } else {
-    BODY.classList.remove(...BODY.classList)
-    BODY.classList.add(`${e.target.value}`)
+    BODY.classList.remove(...BODY.classList);
+    BODY.classList.add(`${e.target.value}`);
   }
-})
+});
 
-const preferredLanguage = navigator.language
-
-if (preferredLanguage === "en-US") {
-  RU.style.display = "none"
-  LANG_SWITCH.textContent = "ENG"
+if (!PREFERRED_LANGUAGE.startsWith("ru")) {
+  RU.style.display = "none";
+  LANG_SWITCH.textContent = "ENG";
 } else {
-  ENG.style.display = "none"
-  LANG_SWITCH.textContent = "RU"
+  ENG.style.display = "none";
+  LANG_SWITCH.textContent = "RU";
 }
 
 LANG_SWITCH.addEventListener("click", (e) => {
   if (e.target.textContent === "ENG") {
-    ENG.style.display = "none"
-    RU.style.display = "block"
-    e.target.textContent = "RU"
+    ENG.style.display = "none";
+    RU.style.display = "block";
+    e.target.textContent = "RU";
   } else {
-    RU.style.display = "none"
-    ENG.style.display = "block"
-    e.target.textContent = "ENG"
+    RU.style.display = "none";
+    ENG.style.display = "block";
+    e.target.textContent = "ENG";
   }
-})
+});
